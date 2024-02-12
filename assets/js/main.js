23f55758d11f35b035b4ca7d24226cf6615886a1
vitorya <graziellevitorya05@gmail.com>
2024-02-12 00:09:06 -0300

    UP archives

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+
+const changeTheme = document.querySelector('.change-theme');
+const divTheme = document.querySelector('.div-theme');
+const navBar = document.querySelector('.navbar');
+const pennywiseMobTheme = document.querySelector('.pennywise-mobile');
+const pennywiseDeskTheme = document.querySelector('.pennywise-desktop');
+const sectionUmTheme = document.querySelector('.section-1');
+const titleTheme = document.querySelector('.title');
+const arrowDonwTheme = document.querySelector('.arrow-donw');
+const sectionDoisTheme = document.querySelector('.section-2');
+const jorgeTheme = document.querySelector('.jorge');
+
+changeTheme.onclick = function(){
+  this.classList.toggle('active')
+  divTheme.classList.toggle('active')
+  pennywiseMobTheme.classList.toggle('active')
+  pennywiseDeskTheme.classList.toggle('active')
+  navBar.classList.toggle('active')
+  sectionUmTheme.classList.toggle('active')
+  titleTheme.classList.toggle('active')
+  arrowDonwTheme.classList.toggle('active')
+  sectionDoisTheme.classList.toggle('active')
+  jorgeTheme.classList.toggle('active')
+}
